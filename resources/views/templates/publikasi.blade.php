2e0ea0b42c47ee446bf19b69cd6df2045b64404f
ilham rizky <ilhamrizkyakbar@gmail.com>
2021-03-26 14:14:52 +0700

    all content done but still hardcoded

--- resources/views/templates/publikasi.blade.php
+++ resources/views/templates/publikasi.blade.php
@@ -17,11 +17,101 @@
 
 <body>
     <div id="app">
-        <div class="publikasi">
-
+        <div class="publikasi" id="publikasi">
+            <h4 style="margin-bottom: 5%">Produk Kami</h4>
+            <div class="flex-container">
+                <div class="publikasi-content flex-child">
+                    <img src="https://www.infobudaya.net/wp-content/uploads/2021/03/hari-raya-nyepi-750x233.jpg"
+                        alt="logo" style="height: 200px; width: 200px; margin-right: 2%;" class="image-left">
+                    <p><strong>INFOBUDAYA</strong></p>
+                    <h3 class="title-publikasi"
+                        onclick="window.open('https://www.infobudaya.net/2021/03/perayaan-nyepi-oleh-desa-padangkerta-karangasem-berbeda-namun-tetap-sarat-makna/','_blank')">
+                        PERAYAAN NYEPI OLEH DESA PADANGKERTA, KARANGASEM: BERBEDA NAMUN TETAP SARAT MAKNA</h3>
+                    <p>
+                        Apa yang kalian pikirkan ketika mendengar kata ‘Nyepi’? Pasti yang terlintas dalam benak kita
+                        adalah kesunyian dan tidak ada yang boleh beraktivitas kan? Namun siapa mengira bahwa di salah
+                        satu desa adat yang betempat di Kabupaten Karangasem, Bali selalu merayakan Hari Raya Nyepi
+                        dengan melakukan aktivitas seperti biasa.
+                    </p>
+                </div>
+                <div class="publikasi-content flex-child">
+                    <img src="https://bfinews.com/resource/cov_clust.png" alt="logo"
+                        style="height: 200px; width: 200px; margin-right: 2%;" class="image-left">
+                    <p><strong>BFINEWS</strong></p>
+                    <h3 class="title-publikasi"
+                        onclick="window.open('https://bfinews.com/news/2021-02-01_jejak_transmisi_komunitas_virus_corona','_blank')">
+                        Jejak Transmisi Komunitas Virus Corona</h3>
+                    <p>
+                        Infeksi ke-nol COVID-19 di Indonesia diduga mendapatkan virus tersebut saat berdansa dengan
+                        seorang Jepang di malam Valentine 14 Februari 2020, yang positif saat diperiksa di Malaysia.
+                        Dari tengah bulan hingga awal Maret 2020 saat kasus pertama tersebut diumumkan, gerbang-gerbang
+                        imigrasi masih terbuka.
+                    </p>
+                </div>
+            </div>
+            <div class="flex-container">
+                <div class="publikasi-content flex-child">
+                    <img src="https://datainsight.id/wp-content/uploads/2021/02/jumat-1.png" alt="logo"
+                        style="height: 200px; width: 200px; margin-right: 2%;" class="image-left">
+                    <p><strong>DATAINSIGHT</strong></p>
+                    <h3 class="title-publikasi"
+                        onclick="window.open('https://datainsight.id/blog/tanggapan-netizen-twitter-mengenai-penggunaan-dinar-dan-dirham-di-indonesia/','_blank')">
+                        Tanggapan Netizen Twitter Mengenai Penggunaan Dinar dan Dirham Di Indonesia.
+                    </h3>
+                    <p>
+                        Sejak tanggal 3 Februari 2021 nama Zaim Saidi mencuat di berbagai lini pemberitaan bahkan juga
+                        ramai di jagat media sosial. Lantas siapakah Zaim Saidi dan apa yang terjadi dengan beliau
+                        sehingga selama 2 hari berturut-turut dirinya menjadi bintang dalam setiap perbincangan di media
+                        sosial khususnya twitter?
+                    </p>
+                </div>
+                <div class="publikasi-content flex-child">
+                    <img src="https://www.infobudaya.net/wp-content/uploads/2021/03/isra-miraj-750x233.jpg" alt="logo"
+                        style="height: 200px; width: 200px; margin-right: 2%;" class="image-left">
+                    <p><strong>INFOBUDAYA</strong></p>
+                    <h3 class="title-publikasi"
+                        onclick="window.open('https://www.infobudaya.net/2021/03/melihat-tradisi-perayaan-isra-miraj-oleh-suku-sasak/','_blank')">
+                        MELIHAT TRADISI PERAYAAN ISRA MI’RAJ OLEH SUKU SASAK</h3>
+                    <p>
+                        Isra Mi’raj merupakan salah satu peristiwa penting bagi umat Islam di seluruh dunia. Tidak
+                        sedikit dari mereka yang merayakan hari besar ini dengan melakukan beberapa tradisi yang telah
+                        dianut turun-temurun.
+                    </p>
+                </div>
+            </div>
+            <div class="flex-container">
+                <div class="publikasi-content flex-child">
+                    <img src="https://bfinews.com/resource/distribusi_media_allignment.png" alt="logo"
+                        style="height: 200px; width: 200px; margin-right: 2%;" class="image-left">
+                    <p><strong>BFINEWS</strong></p>
+                    <h3 class="title-publikasi"
+                        onclick="window.open('https://bfinews.com/news/2020-12-01_quo_vadis_kredibilitas_media_berita','_blank')">
+                        Quo Vadis Kredibilitas Media Berita</h3>
+                    <p>
+                        Perkembangan media daring dan media sosial dalam beberapa tahun terakhir ini telah mengubah
+                        secara radikal cara publik mengkonsumsi informasi. Survei menunjukan bahwa 63% publik
+                        mengkonsumsi berita secara digital, sementara jejaring sosial seperti Twitter dan Facebook
+                        menjadi tempat dimana publik menerima, berbagi dan mendiskusikan berita berbagai perkembangan
+                        terbaru.
+                    </p>
+                </div>
+                <div class="publikasi-content flex-child">
+                    <img src="https://datainsight.id/wp-content/uploads/2021/01/Green-Organic-Natural-Photosynthesis-Biology-Infographic-1-1.png"
+                        alt="logo" style="height: 200px; width: 200px; margin-right: 2%;" class="image-left">
+                    <p><strong>DATAINSIGHT</strong></p>
+                    <h3 class="title-publikasi"
+                        onclick="window.open('https://datainsight.id/blog/berawal-dari-kekesalan-ambroncius-melakukan-tindakan-rasis-kepada-natalius-pigai/','_blank')">
+                        Berawal Dari Kekesalan, Ambroncius Melakukan Tindakan Rasis Kepada Natalius Pigai</h3>
+                    <p>
+                        “NKRI Harga Mati” inilah slogan yang selalu diserukan oleh bangsa Indonesia. Namun apalah arti
+                        slogan ini jika masih saja terjadi rasisme yang ditujukan kepada saudara-saudara kita terlebih
+                        lagi mereka yang berada di ujung timur Indonesia?
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
-    
+
     @include('templates.partials.footer')
     <script src="{{ asset('js/app.js') }}"></script>
 
